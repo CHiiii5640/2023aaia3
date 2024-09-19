cd4a8f5f1c9514df43e0a2d5fa9d85ca30f12a9d
CHiiii5640 <12750211@me.mcu.edu.tw>
2024-09-19 16:24:31 +0800

    week02

---
+++ week02/week02-4.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int H[265]={};
+        for(char c:s)
+        {
+            H[c]++;
+        }
+
+        for(char c:t)
+        {
+            H[c]--;
+            if(H[c]<0)
+                return c;
+        }
+        return ' ';
+    }
+};
